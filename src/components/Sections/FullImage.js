88904dcac3e13e03bbcb499d485f22f1dfac1920
mordonez <mordonez.sanchez@gmail.com>
2022-01-12 00:44:22 +0100

    Removes unnecesary FullWidthImage

--- src/components/Sections/FullImage.js
+++ src/components/Sections/FullImage.js
@@ -1,12 +1,56 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import FullWidthImage from "@components/FullWidthImage";
-import { getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const FullImage = ({ data }) => {
   const image = getImage(data.image)
   return (
-    <FullWidthImage image={image || { url: data.image} } height={data.height} heading={data.heading} subheading={data.subheading} imagePosition="center"/>
+    <div
+      className="mt-0"
+      style={{
+        display: "grid",
+        alignItems: "center",
+      }}
+    >
+      <GatsbyImage
+            image={image}
+            objectFit={"cover"}
+            objectPosition="center"
+            style={{
+              gridArea: "1/1",
+              maxHeight: data.height ? data.height : "300",
+            }}
+            layout="fullWidth"
+            aspectratio={3 / 1}
+            alt=""
+            formats={["auto", "webp", "avif"]}
+          />
+        {(data.heading || data.subheading) && (
+          <div
+            style={{
+              gridArea: "1/1",
+              position: "relative",
+              placeItems: "center",
+              display: "grid",
+            }}
+          >
+            {data.heading && (
+              <h1
+                className="has-background-primary p-1 has-text-white has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+              >
+              {data.heading}
+              </h1>
+            )}
+            {data.subheading && (
+              <h3
+                className="has-background-primary has-text-white p-1 mt-2 has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+              >
+                {data.subheading}
+              </h3>
+            )}
+          </div>
+        )}
+      </div>
   )
 } ;
 
